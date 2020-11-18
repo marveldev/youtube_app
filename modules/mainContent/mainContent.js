cbf0731c0477cc68f087160a4cd4490887e96ca2
Marvel Dev <marvellousalika48@gmail.com>
2020-11-18 04:49:40 +0100

    add maincontent

--- modules/mainContent/mainContent.js
+++ modules/mainContent/mainContent.js
@@ -1,7 +1,36 @@
 const mainContent = () => {
-  <div>
-
-  </div>
+  return `
+    <div class="main-content">
+      <nav class="button-nav">
+        <button>All</button>
+        <button>Odulade Adekola</button>
+        <button>Shepard Bushiri</button>
+        <button>JavaScript</button>
+        <button>Css</button>
+        <button>React</button>
+        <button>Python</button>
+        <button>Computer</button>
+        <button>Music</button>
+      </nav>
+      <div class="video-gallery">
+        <div>
+          <video>
+            <source src="https://player.vimeo.com/external/350245348.sd.mp4?s=5a4dafd828188844cef2274dd0bc8babb7d06687&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+        </div>
+        <div>
+          <video>
+            <source src="https://player.vimeo.com/external/316817750.sd.mp4?s=cecc367281d676b407a94a1470f5c4e4dd4aa4f1&profile_id=164&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+        </div>
+        <div>
+          <video>
+            <source src="https://player.vimeo.com/external/374119382.sd.mp4?s=2e2cf9aa0bd93c53987df1a2b07240602942ce72&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+        </div>
+      </div>
+    </div>
+  `
 }
 
-<!-- export default mainContent; -->
+export default mainContent;

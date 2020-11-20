72a18264c7aaaf10276f03d9f9a286c1bc83c1e9
Marvel Dev <marvellousalika48@gmail.com>
2020-11-20 16:17:25 +0100

    initial commit

---
+++ modules/rightNav/rightNavModal.js
@@ -0,0 +1,23 @@
+const rightNavModal = () => {
+  return `
+    <nav class="rightNav-modal">
+      <a href="#">
+        <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
+        class="image" alt="photo">
+        <strong>Jane Doe</strong>
+      </a>
+      <a href="#"><i class="fa fa-user-circle"></i><strong>Your channel</strong></a>
+      <a href="#"><i class='fas fa-dollar-sign'></i><strong>Purchases and memberships</strong></a>
+      <a href="#"><i class='fas fa-users-cog'></i><strong>Switch account</strong></a>
+      <a href="#"><i class='fas fa-sign-out-alt'></i><strong>Sign out</strong></a>
+      <a href="#"><i class="material-icons">&#xe8bd;</i><strong>Appearance: Light</strong>
+      <i class="material-icons">&#xe315;</i></a>
+      <a href="#"><i class="material-icons">&#xe8b8;</i><strong>Settings</strong></a>
+      <a href="#"><i class='fas fa-question-circle'></i><strong>Help</strong></a>
+      <a href="#"><i class="material-icons">&#xe87f;</i><strong>Send feedback</strong></a>
+
+    </nav>
+  `
+}
+
+export default rightNavModal;

121bfcec834afcaba30e742573cd184af915360e
Marvel Dev <marvellousalika48@gmail.com>
2020-11-21 08:01:08 +0100

    add toggle theme

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import mainContent from './modules/mainContent/mainContent.js';
 import { videoEventListeners } from './modules/mainContent/events.js';
 import leftNavEventListeners from './modules/leftNav/events.js';
 import rightNavModal from './modules/rightNav/rightNavModal.js';
-import topNavEvents from './modules/topNav/events.js';
+import{ topNavEvents, toggleTheme } from './modules/topNav/events.js';
 
 
 const app = () => {
@@ -22,3 +22,4 @@ document.querySelector('#root').innerHTML = app();
 videoEventListeners();
 leftNavEventListeners();
 topNavEvents();
+toggleTheme();

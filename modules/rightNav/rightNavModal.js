8a2b06b961a282e665976dc96ff3b73c6a1a1466
Marvel Dev <marvellousalika48@gmail.com>
2020-11-21 05:46:56 +0100

    add cleaner code

--- modules/rightNav/rightNavModal.js
+++ modules/rightNav/rightNavModal.js
@@ -1,22 +1,30 @@
 const rightNavModal = () => {
   return `
-    <nav class="rightNav-modal">
-      <a href="#">
-        <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
-        class="image" alt="photo">
-        <strong>Jane Doe</strong>
-      </a>
-      <a href="#"><i class="fa fa-user-circle"></i><strong>Your channel</strong></a>
-      <a href="#"><i class='fas fa-dollar-sign'></i><strong>Purchases and memberships</strong></a>
-      <a href="#"><i class='fas fa-users-cog'></i><strong>Switch account</strong></a>
-      <a href="#"><i class='fas fa-sign-out-alt'></i><strong>Sign out</strong></a>
-      <a href="#"><i class="material-icons">&#xe8bd;</i><strong>Appearance: Light</strong>
-      <i class="material-icons">&#xe315;</i></a>
-      <a href="#"><i class="material-icons">&#xe8b8;</i><strong>Settings</strong></a>
-      <a href="#"><i class='fas fa-question-circle'></i><strong>Help</strong></a>
-      <a href="#"><i class="material-icons">&#xe87f;</i><strong>Send feedback</strong></a>
-
-    </nav>
+    <div id="overlay"></div>
+    <div id="rightNav">
+      <nav class="rightNav-modal">
+        <div>
+          <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
+          class="image" alt="photo">
+          <strong id="userName">Jane Doe</strong>
+        </div>
+        <a href="#"><i class="fa fa-user-circle"></i>Your channel</a>
+        <a href="#"><i class='fas fa-dollar-sign'></i>Purchases and memberships</a>
+        <a href="#"><i class='fas fa-users-cog'></i>Switch account</a>
+        <a href="#"><i class='fas fa-sign-out-alt'></i>Sign out</a>
+        <a href="#" id="theme"><i class="material-icons">&#xe8bd;</i>Appearance: Light
+          <i class="material-icons">&#xe315;</i>
+        </a>
+        <a href="#"><i class="material-icons">&#xe8b8;</i>Settings</a>
+        <a href="#"><i class='fas fa-question-circle'></i>Help</a>
+        <a href="#"><i class="material-icons">&#xe87f;</i>Send feedback</a>
+      </nav>
+      <div id="themeModal">
+        <p><i id="arrowLeft" class='fas fa-arrow-left'></i>Appearance</p>
+        <a href="#" id="darkTheme"><i id="checkmark" class='fas fa-check'></i>Dark theme</a>
+        <a href="#" id="lightTheme"><i class='fas fa-check'></i>Light theme</a>
+      </div>
+    </div>
   `
 }
 

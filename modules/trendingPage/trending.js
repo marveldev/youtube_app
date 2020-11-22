7d383949301d6237c77fae4b20b3320d79a5a2d0
Marvel Dev <marvellousalika48@gmail.com>
2020-11-22 07:12:13 +0100

    initial commit

---
+++ modules/trendingPage/trending.js
@@ -0,0 +1,26 @@
+const trendingPage = () => {
+  return `
+    <div class="hello">
+      <div>
+        <a href="#">
+          <img src="https://youtube.com/img/trending/chips/music_80x80.png" alt="">
+          <span>Music</span>
+        </a>
+        <a href="#">
+          <img src="https://youtube.com/img/trending/chips/gaming_80x80.png" alt="">
+          <span>Gaming</span>
+        </a>
+        <a href="#">
+          <img src="https://youtube.com/img/trending/chips/news_80x80.png" alt="">
+          <span>News</span>
+        </a>
+        <a href="#">
+          <img src="https://youtube.com/img/trending/chips/movies_80x80.png" alt="">
+          <span>Movies</span>
+        </a>
+      </div>
+    </div>
+  `
+}
+
+export default trendingPage;

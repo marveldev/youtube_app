ebdc5e263ade784c89ed621f775666a47abc7a29
Marvel Dev <marvellousalika48@gmail.com>
2020-11-23 18:29:59 +0100

    add trending video

--- modules/trendingPage/trending.js
+++ modules/trendingPage/trending.js
@@ -1,7 +1,7 @@
 const trendingPage = () => {
   return `
-    <div class="hello">
-      <div>
+    <div class="trending-page">
+      <div class="trending-nav">
         <a href="#">
           <img src="https://youtube.com/img/trending/chips/music_80x80.png" alt="">
           <span>Music</span>
@@ -18,6 +18,76 @@ const trendingPage = () => {
           <img src="https://youtube.com/img/trending/chips/movies_80x80.png" alt="">
           <span>Movies</span>
         </a>
+      </div>
+      <div id="trends">
+        <div class="trending-video">
+          <video class="video">
+            <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+          <div>
+            <div>
+              <h3>The Chinese Culture</h3>
+              <span>Pexels Video</span>
+              <span>28k views</span>
+              <span><strong>&middot</strong> 8 months ago</span>
+              <p>Ronaldo scores the victory for Juventus with two goals in the first half. 
+                Serie A TIM This is the official channel for the Serie A, providing all the 
+                latest highlights, interviews, news...
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="trending-video">
+          <video class="video">
+            <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+          <div>
+            <div>
+              <h3>The Chinese Culture</h3>
+              <span>Pexels Video</span>
+              <span>28k views</span>
+              <span><strong>&middot</strong> 8 months ago</span>
+              <p>Ronaldo scores the victory for Juventus with two goals in the first half. 
+                Serie A TIM This is the official channel for the Serie A, providing all the 
+                latest highlights, interviews, news...
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="trending-video">
+          <video class="video">
+            <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+          <div>
+            <div>
+              <h3>The Chinese Culture</h3>
+              <span>Pexels Video</span>
+              <span>28k views</span>
+              <span><strong>&middot</strong> 8 months ago</span>
+              <p>Ronaldo scores the victory for Juventus with two goals in the first half. 
+                Serie A TIM This is the official channel for the Serie A, providing all the 
+                latest highlights, interviews, news...
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="trending-video">
+          <video class="video">
+            <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          </video>
+          <div>
+            <div>
+              <h3>The Chinese Culture</h3>
+              <span>Pexels Video</span>
+              <span>28k views</span>
+              <span><strong>&middot</strong> 8 months ago</span>
+              <p>Ronaldo scores the victory for Juventus with two goals in the first half. 
+                Serie A TIM This is the official channel for the Serie A, providing all the 
+                latest highlights, interviews, news...
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `

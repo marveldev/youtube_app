ed0d397117faf4d6e9053ab293eeebfe224f5f21
Marvel Dev <marvellousalika48@gmail.com>
2020-11-25 06:53:33 +0100

    add home page

--- modules/activePage/events.js
+++ modules/activePage/events.js
@@ -1,28 +1,30 @@
+import homePage from '../homePage/homePage.js';
 import trendingPage from '../trendingPage/trending.js';
-import channelPage from '../channelPage/channel.js';
 import watchHistoryPage from '../watchHisoryPage/watchHistory.js';
+import channelPage from '../channelPage/channel.js';
 
 const togglePages = () => {
   const activePage = document.querySelector('.active-page');
 
+  const homeButton = document.querySelector('.history');
+  homeButton.addEventListener('click', () => {
+    activePage.innerHTML = `${homePage()}`;
+  })
+
   const trendingButton = document.querySelector('#trending');
   trendingButton.addEventListener('click', () => {
     activePage.innerHTML = `${trendingPage()}`;
-    console.log(activePage);
+  })
+  
+  const historyButton = document.querySelector('#history');
+  historyButton.addEventListener('click', () => {
+    activePage.innerHTML = `${watchHistoryPage()}`;
   })
 
   const channelButton = document.querySelector('#channel');
   channelButton.addEventListener('click', () => {
     activePage.innerHTML = `${channelPage()}`;
-    console.log(activePage);
   })
-
-  const historyButton = document.querySelector('#history');
-  historyButton.addEventListener('click', () => {
-    activePage.innerHTML = `${watchHistoryPage()}`;
-    console.log(activePage);
-  })
-
 }
 
 export default togglePages;

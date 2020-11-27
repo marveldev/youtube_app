edddc1c5716bdb4c05ff9036c91cb79d7967b296
Marvel Dev <marvellousalika48@gmail.com>
2020-11-27 06:08:54 +0100

    deleted

--- modules/activePage/activePage.js
+++
@@ -1,9 +0,0 @@
-import homePage from '../homePage/homePage.js';
-
-const activePage = () => {
-  return `
-    <section class="active-page">${homePage()}</section>
-  `
-}
-
-export default activePage;

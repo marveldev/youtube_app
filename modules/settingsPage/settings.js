cf9950412ed99fb3a4a2fdad34b956a2f17300aa
Marvel Dev <marvellousalika48@gmail.com>
2020-11-30 07:01:40 +0100

    initial commit

---
+++ modules/settingsPage/settings.js
@@ -0,0 +1,21 @@
+const settings = () => {
+  return `
+    <div>
+      <nav class="leftside-nav settings">
+        <p>SETTTINGS</p>
+        <a href="#">Account</a>
+        <a href="#">Notification</a>
+        <a href="#">Playback and performance</a>
+        <a href="#">Privacy</a>
+        <a href="#">Connected apps</a>
+        <a href="#">Billings and payments</a>
+        <a href="#">Advanced settings</a>
+      </nav>
+    </div>
+    <div>
+      <p>Hello</p>
+    </div>
+  `
+}
+
+export default settings;

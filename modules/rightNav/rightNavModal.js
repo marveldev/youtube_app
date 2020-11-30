61e02bcc21b01f7c5770fd7055991e996d14cba4
Marvel Dev <marvellousalika48@gmail.com>
2020-11-30 07:01:28 +0100

    add close modal button

--- modules/rightNav/rightNavModal.js
+++ modules/rightNav/rightNavModal.js
@@ -7,6 +7,7 @@ const rightNavModal = () => {
           <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
           class="image" alt="photo">
           <strong id="userName">Jane Doe</strong>
+          <button id="closeModalButton">X</button>
         </div>
         <a href="#" id="channel"><i class="fa fa-user-circle"></i>Your channel</a>
         <a href="#"><i class='fas fa-dollar-sign'></i>Purchases and memberships</a>

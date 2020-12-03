9b80b6b1b228ddc1c6ac2315786bc890f6af5e43
Marvel Dev <marvellousalika48@gmail.com>
2020-12-03 23:58:01 +0100

    initial commit

---
+++ modules/helpers.js
@@ -0,0 +1,14 @@
+import homePage from './defaultPage/homePage/homePage.js';
+import channelPage from './defaultPage/channelPage/channel.js';
+import homeLeftNav from './defaultPage/leftNav/leftNav.js';
+import trendingPage from './defaultPage/trendingPage/trending.js';
+import watchHistoryPage from './defaultPage/watchHisoryPage/watchHistory.js';
+import rightNav from './rightNav/rightNavModal.js';
+import topNav from './topNav/topNav.js';
+import settingsNav from './settingsPage/leftNav/settingsNav.js';
+import accountPage from './settingsPage/content/accountPage.js';
+
+
+const switchActivePage = (page) => {
+
+}

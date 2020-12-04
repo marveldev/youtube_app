6fa6e4c258b45656fdb64303dd91ed0872f8cfd0
Marvel Dev <marvellousalika48@gmail.com>
2020-12-04 22:06:23 +0100

    remove unused imports.

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 import topNav from './modules/topNav/topNav.js';
-import { homeContent, homeEventListeners } from './modules/defaultPage/homeContent.js';
 import rightNavModal from './modules/rightNav/rightNavModal.js';
-import { videoEventListeners } from './modules/defaultPage/homePage/events.js';
 import leftNavEventListeners from './modules/defaultPage/leftNav/events.js';
-import{ topNavEvents } from './modules/topNav/events.js';
 import leftSideNav from './modules/defaultPage/leftNav/leftNav.js';
-import homePage from './modules/defaultPage/homePage/homePage.js';
-// import togglePages from './modules/activePage/events.js';
-import { settingsContent, settingEventListeners } from './modules/settingsPage/settingsContent.js';
 import { switchCurrentPage } from './modules/helpers.js';
+import { topNavEvents } from './modules/topNav/events.js';
 
 const app = () => {
   return `
     ${topNav()}
-    <section class="left-side-nav">
-      ${leftSideNav()}
-    </section>
-    <section class="current-page">
-    </section>
-    <section id="page">
-      ${settingsContent()}
+    <section class="grid-container">
+      <section class="side-nav">
+        ${leftSideNav()}
+      </section>
+      <section class="current-page">
+      </section>
     </section>
     ${rightNavModal()}
   `
@@ -28,10 +22,6 @@ const app = () => {
 document.querySelector('#root').innerHTML = app();
 
 leftNavEventListeners();
+topNavEvents();
 const currentPage = localStorage.getItem('currentPage');
 switchCurrentPage(currentPage || 'homePage');
-
-// videoEventListeners();
-// topNavEvents();
-// homeEventListeners();
-// settingEventListeners();

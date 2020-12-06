a37c5806f89f04e865f2320df322b8ce8677b398
Marvel Dev <marvellousalika48@gmail.com>
2020-12-06 07:14:18 +0100

    -remove burgericon function -add switchPages function

--- modules/defaultPage/leftNav/events.js
+++ modules/defaultPage/leftNav/events.js
@@ -1,28 +1,21 @@
 import { switchCurrentPage, constants } from '../../helpers.js';
 
 const leftNavEventListeners = () => {
-  const burgerIcon = document.querySelector('.burger-icon');
-  const leftNav = document.querySelector('.leftside-nav');
-
-  burgerIcon.addEventListener('click', () => {
-    if (leftNav.style.display === 'none') {
-      leftNav.style.display = 'block';
-      document.querySelector('#leftSideNav').style.display = 'none';
-      document.querySelector('.home-container').classList.remove('wide');
-    } else {
-      leftNav.style.display = 'none';
-      document.querySelector('#leftSideNav').style.display = 'block';
-      document.querySelector('.home-container').classList.add('wide');
+  const switchPages = (selectors, constant) => {
+    for (let index = 0; index < selectors.length; index++) {
+      const selector = selectors[index];
+      selector.addEventListener('click', () => {
+        switchCurrentPage(constant);
+      })
     }
-  })
+  }
+  switchPages(document.querySelectorAll('.home'), constants.HOMEPAGE);
+  switchPages(document.querySelectorAll('.trending'), constants.TRENDINGPAGE);
+  // switchPages(document.querySelectorAll('.home'), constants.HOMEPAGE);
 
-  const homePage = document.querySelector('.home');
-  const trendingPage = document.querySelector('.trending');
   const historyPage = document.querySelector('.history');
   const settingsNav = document.querySelector('.setting');
 
-  homePage.addEventListener('click', () => switchCurrentPage(constants.HOMEPAGE));
-  trendingPage.addEventListener('click', () => switchCurrentPage(constants.TRENDINGPAGE));
   historyPage.addEventListener('click', () => switchCurrentPage(constants.WATCHHISTORYPAGE));
   settingsNav.addEventListener('click', () => switchCurrentPage(constants.SETTINGSPAGE));
 }

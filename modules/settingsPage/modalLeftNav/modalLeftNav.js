a812ad139bf90e31d3beb0069330bab0f3bee30a
Marvel Dev <marvellousalika48@gmail.com>
2020-12-08 21:41:13 +0100

    change anchor tag to button

--- modules/settingsPage/modalLeftNav/modalLeftNav.js
+++ modules/settingsPage/modalLeftNav/modalLeftNav.js
@@ -1,62 +1,62 @@
 const modalLeftNav = () => {
   return `
     <nav class="modal-left-nav">
-      <span>
-        <a href="#" id="burgerIcon"><i class='fas fa-bars'></i></a>
-        <a href="#" class="logo"><i class='fab fa-youtube'></i>
+      <section>
+        <button id="burgerIcon"><i class='fas fa-bars'></i></button>
+        <button><i class='fab fa-youtube'></i>
           <strong>YouTube</strong><small><sup>NG</sup></small>
-        </a>
-      </span>
+        </button>
+      </section>
       <div>
-        <a href="#" class="home"><i class="material-icons">&#xe88a;</i><strong>Home</strong></a>
-        <a href="#" class="icon trending"><i class='fas fa-fire'></i><strong>Trending</strong></a>
-        <a href="#"><i class="material-icons">&#xe064;</i><strong>Subscriptions</strong></a>
+        <button class="home"><i class="material-icons">&#xe88a;</i><strong>Home</strong></button>
+        <button class="icon trending"><i class='fas fa-fire'></i><strong>Trending</strong></button>
+        <button><i class="material-icons">&#xe064;</i><strong>Subscriptions</strong></button>
       </div>
       <div>
-        <a href="#"><i class="material-icons">&#xe04a;</i><strong>Library</strong></a>
-        <a href="#" class="icon history"><i class="fa fa-history"></i><strong>History</strong></a>
-        <a href="#"><i class="material-icons">&#xe54d;</i><strong>Your movies</strong></a>
-        <a href="#"class="setting"><i class="material-icons">&#xe8b8;</i>Settings</a>
-        <a href="#" class="icon"><i class='fas fa-clock'></i><strong>Watch later</strong></a>
+        <button><i class="material-icons">&#xe04a;</i><strong>Library</strong></button>
+        <button class="icon history"><i class="fa fa-history"></i><strong>History</strong></button>
+        <button><i class="material-icons">&#xe54d;</i><strong>Your movies</strong></button>
+        <button class="setting"><i class="material-icons">&#xe8b8;</i>Settings</button>
+        <button class="icon"><i class='fas fa-clock'></i><strong>Watch later</strong></button>
       </div>
       <div>
         <strong class="user-subscription">SUBSCRIPTIONS</strong>
         <div class="sub-list">
-          <a href="#">
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Jack Williams</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Dangote Buka</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Jude Okolie</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Mary Spencer</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Tobias Flench</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>John White</strong>
-          </a>
-          <a href="#">
+          </button>
+          <button>
             <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <strong>Lilly Little </strong>
-          </a>
+          </button>
         </div>
       </div>
     </nav>

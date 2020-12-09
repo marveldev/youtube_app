7b0df9da32465bc25e68f133c1abab6199ae70ca
Marvel Dev <marvellousalika48@gmail.com>
2020-12-09 05:35:01 +0100

    add a function to switchPages

--- modules/settingsPage/settingsLeftNav/events.js
+++ modules/settingsPage/settingsLeftNav/events.js
@@ -1,5 +1,13 @@
+import { switchCurrentPage, constants } from '../../helpers.js';
+
 const settingsNavEventListeners = () => {
-  
+  const switchPages = (selector, constant) => {
+    selector.addEventListener('click', () => {
+      switchCurrentPage(constant);
+    })
+  }
+  switchPages(document.querySelector('.account'), constants.ACCOUNTPAGE);
+  switchPages(document.querySelector('.notification'), constants.NOTIFICATIONPAGE);
 }
 
 export default settingsNavEventListeners;

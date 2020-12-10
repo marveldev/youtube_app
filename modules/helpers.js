534ade69ba15ea1cc13f708c267396cd31b18f6d
Marvel Dev <marvellousalika48@gmail.com>
2020-12-10 06:14:59 +0100

    add display for performancePage

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,4 +1,3 @@
-import topNav from './topNav/topNav.js';
 import defaultLeftNav from './homePage/defaultLeftNav/defaultLeftNav.js';
 import defaultPage from './homePage/defaultPage/defaultPage.js';
 import channelPage from './homePage/channelPage/channelPage.js';
@@ -6,27 +5,23 @@ import trendingPage from './homePage/trendingPage/trendingPage.js';
 import watchHistoryPage from './homePage/watchHisoryPage/watchHistoryPage.js';
 import settingsNav from './settingsPage/settingsLeftNav/settingsNav.js';
 import accountPage from './settingsPage/accountPage/accountPage.js';
-import { topNavEventListeners } from './topNav/events.js';
-import defaultNavEventListeners from './homePage/defaultLeftNav/events.js';
 import notificationPage from './settingsPage/notificationPage/notificationPage.js';
 import settingsNavEventListeners from './settingsPage/settingsLeftNav/events.js';
+import performancePage from './settingsPage/performancePage/performancePage.js';
 
 const constants = {
   HOMEPAGE: 'homePage',
   CHANNELPAGE: 'channelPage',
-  HOMELEFTNAV: 'homeLeftNav',
   TRENDINGPAGE: 'trendingPage',
   WATCHHISTORYPAGE: 'watchHistoryPage',
-  RIGHTNAV: 'rightNav',
   DEFAULTNAV: 'defaultNav',
   SETTINGSNAV : 'settingsNav',
-  SETTINGSPAGE: 'settingsPage',
   ACCOUNTPAGE: 'accountPage',
-  NOTIFICATIONPAGE: 'notificationPage'
+  NOTIFICATIONPAGE: 'notificationPage',
+  PERFORMANCEPAGE: 'performancePage'
 }
 
 const switchCurrentPage = page => {
-  const topHomeNav = document.querySelector('.top-nav-content');
   const currentPage = document.querySelector('.current-page');
   const currentSideNav = document.querySelector('.side-nav');
   switch(page) {
@@ -66,6 +61,10 @@ const switchCurrentPage = page => {
     case 'notificationPage':
       currentPage.innerHTML = notificationPage();
       localStorage.setItem('currentPage', constants.NOTIFICATIONPAGE)
+      break;
+    case 'performancePage':
+      currentPage.innerHTML = performancePage();
+      localStorage.setItem('currentPage', constants.PERFORMANCEPAGE)
       break;
     default:
       currentPage.innerHTML = defaultPage();

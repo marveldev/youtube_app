66b12b2f72c8baa7e315769ef5a7cae60928c97c
Marvel Dev <marvellousalika48@gmail.com>
2020-12-12 06:57:42 +0100

    remove class name

--- modules/settingsPage/modalLeftNav/modalLeftNav.js
+++ modules/settingsPage/modalLeftNav/modalLeftNav.js
@@ -8,15 +8,15 @@ const modalLeftNav = () => {
         </button>
       </section>
       <div>
-        <button class="home"><i class="material-icons">&#xe88a;</i><strong>Home</strong></button>
-        <button class="icon trending"><i class='fas fa-fire'></i><strong>Trending</strong></button>
+        <button><i class="material-icons">&#xe88a;</i><strong>Home</strong></button>
+        <button class="icon"><i class='fas fa-fire'></i><strong>Trending</strong></button>
         <button><i class="material-icons">&#xe064;</i><strong>Subscriptions</strong></button>
       </div>
       <div>
         <button><i class="material-icons">&#xe04a;</i><strong>Library</strong></button>
-        <button class="icon history"><i class="fa fa-history"></i><strong>History</strong></button>
+        <button class="icon"><i class="fa fa-history"></i><strong>History</strong></button>
         <button><i class="material-icons">&#xe54d;</i><strong>Your movies</strong></button>
-        <button class="setting"><i class="material-icons">&#xe8b8;</i>Settings</button>
+        <button><i class="material-icons">&#xe8b8;</i>Settings</button>
         <button class="icon"><i class='fas fa-clock'></i><strong>Watch later</strong></button>
       </div>
       <div>

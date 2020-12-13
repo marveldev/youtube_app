7e2ad9a8d8847706988dc6eab8288b58c9cea003
Marvel Dev <marvellousalika48@gmail.com>
2020-12-13 07:47:28 +0100

    add previouspage button eventlistener

--- modules/settingsPage/settingsLeftNav/events.js
+++ modules/settingsPage/settingsLeftNav/events.js
@@ -1,14 +1,26 @@
 import { switchCurrentPage, constants } from '../../helpers.js';
+import defaultLeftNav from '../../homePage/defaultLeftNav/defaultLeftNav.js';
+import defaultNavEventListeners from '../../homePage/defaultLeftNav/events.js';
 
 const settingsNavEventListeners = () => {
-  const switchPages = (selector, constant) => {
+  const addSwitchPageEvent = (selector, constant) => {
     selector.addEventListener('click', () => {
       switchCurrentPage(constant);
     })
   }
-  switchPages(document.querySelector('.account'), constants.ACCOUNTPAGE);
-  switchPages(document.querySelector('.notification'), constants.NOTIFICATIONPAGE);
-  switchPages(document.querySelector('.performance'), constants.PERFORMANCEPAGE);
+  addSwitchPageEvent(document.querySelector('.account'), constants.ACCOUNTPAGE);
+  addSwitchPageEvent(document.querySelector('.notification'), constants.NOTIFICATIONPAGE);
+  addSwitchPageEvent(document.querySelector('.performance'), constants.PERFORMANCEPAGE);
+
+  const previousPageButton = document.querySelector('#previousPageButton');
+  previousPageButton.addEventListener('click', () => {
+    const currentSideNav = document.querySelector('.side-nav');
+    currentSideNav.innerHTML = defaultLeftNav();
+    console.log('ok');
+    const previousPage = localStorage.getItem('previousPage')
+    switchCurrentPage(previousPage)
+  })
+
 }
 
 export default settingsNavEventListeners;

4c808de1cc0de9290245c5504fe5429f6565373c
Marvel Dev <marvellousalika48@gmail.com>
2020-12-13 07:45:47 +0100

    remove condition statement

--- modules/rightNav/events.js
+++ modules/rightNav/events.js
@@ -3,12 +3,7 @@ import { switchCurrentPage, constants } from '../helpers.js';
 const rightNavEventListeners = () => {
   const channelPage = document.querySelector('.channel');
   channelPage.addEventListener('click', () => {
-    const currentNav = localStorage.getItem('currentNav');
-    if (currentNav === constants.SETTINGSNAV) {
-      switchCurrentPage(constants.DEFAULTCHANNELPAGE);
-    } else {
-      switchCurrentPage(constants.CHANNELPAGE);
-    }
+    switchCurrentPage(constants.CHANNELPAGE);
   })
 }
 

9efad55dbc9e7e6acc9c97b0b4ed3d407e1e629d
Marvel Dev <marvellousalika48@gmail.com>
2020-12-13 20:57:36 +0100

    initial commit

---
+++ modules/videosPage/videoPage.js
@@ -0,0 +1,98 @@
+const videoPageEventListeners = () => {
+  return `
+    <div class="video-gallery">
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>The Chinese Culture</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/350245348.sd.mp4?s=5a4dafd828188844cef2274dd0bc8babb7d06687&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>A Moving Truck</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/350245348.sd.mp4?s=5a4dafd828188844cef2274dd0bc8babb7d06687&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>A Moving Truck</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/374119382.sd.mp4?s=2e2cf9aa0bd93c53987df1a2b07240602942ce72&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>Handshake with friends</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/331083783.sd.mp4?s=d817f766244c10a344e39045edf595366cb72979&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>Enjoy butterfly nature magic</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+      <div class="video-content">
+        <video class="video">
+          <source src="https://player.vimeo.com/external/363169958.sd.mp4?s=2bcfbdfcb641cd1668ea8cee4f038420d8b5cf8c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        </video>
+        <div class="about-video">
+          <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="image" alt="photo">
+          <div>
+            <strong>On tour with friends</strong>
+            <p>Pexels Video</p>
+            <span>28k views</span>
+            <span><strong>&middot</strong> 8 months ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+export default videoPageEventListeners;

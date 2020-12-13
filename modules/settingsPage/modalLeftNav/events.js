a0f5be203667c01dfbcabfbe5016f599500ea324
Marvel Dev <marvellousalika48@gmail.com>
2020-12-13 05:33:43 +0100

    delete file

--- modules/settingsPage/modalLeftNav/events.js
+++
@@ -1,8 +0,0 @@
-const modalLeftNavEventListeners = () => {
-  document.querySelector('#burgerIcon').addEventListener('click', () => {
-    overlay.style.display = 'none';
-    document.querySelector('.modal-left-nav').style.display = 'none';
-  })
-}
-
-export default modalLeftNavEventListeners;

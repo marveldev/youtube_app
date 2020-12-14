453575fe1437ace8316d8ce4e69d6fc2ac5c8c96
Marvel Dev <marvellousalika48@gmail.com>
2020-12-14 23:42:22 +0100

    import switch current page and constant function

--- modules/homePage/defaultPage/events.js
+++ modules/homePage/defaultPage/events.js
@@ -1,11 +1,11 @@
+import { constants, switchCurrentPage } from "../../helpers.js";
+
 const homeEventListeners = () => {
   const videos = document.querySelectorAll('.video');
   for (let index = 0; index < videos.length; index++) {
     const video = videos[index];
     video.addEventListener('click', () => {
-      document.querySelector('.grid-container').style.display = 'none';
-      document.querySelector('.video-page').style.display = 'block';
-
+      switchCurrentPage(constants.VIDEOPAGE);
     })
   }
 }

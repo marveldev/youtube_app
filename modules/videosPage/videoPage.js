d98cdc400f6aa2bc4ef77667fbf9158779e3c18a
Marvel Dev <marvellousalika48@gmail.com>
2020-12-15 17:43:13 +0100

    add src file to video tag

--- modules/videosPage/videoPage.js
+++ modules/videosPage/videoPage.js
@@ -2,8 +2,7 @@ const videoWatchPage = () => {
   return `
     <section class="video-page-container">
       <div id="videoWide">
-        <video class="video-wide">
-          <source src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        <video class="video-wide" src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
         </video>
         <div class="about-video">
         </div>

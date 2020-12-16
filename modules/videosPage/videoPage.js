589961ca9edf7d05f5b4a0e68220702d8bec2a80
Marvel Dev <marvellousalika48@gmail.com>
2020-12-16 07:50:44 +0100

    add src file to video tag

--- modules/videosPage/videoPage.js
+++ modules/videosPage/videoPage.js
@@ -2,7 +2,7 @@ const videoWatchPage = () => {
   return `
     <section class="video-page-container">
       <div>
-        <video class="video-wide" src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+        <video class="video-wide" src="https://player.vimeo.com/external/362720873.sd.mp4?s=0bd51a6eac8fbcb012d1a01d78f036d97f68081c&profile_id=139&oauth2_token_id=57447761" type="video/mp4" controls>
         </video>
         <div>
           <div class="video-info">
@@ -34,8 +34,7 @@ const videoWatchPage = () => {
       </div>
       <div>
         <div id="videoContent">
-          <video>
-            <source src="https://player.vimeo.com/external/350245348.sd.mp4?s=5a4dafd828188844cef2274dd0bc8babb7d06687&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          <video class="video" src="https://player.vimeo.com/external/350245348.sd.mp4?s=5a4dafd828188844cef2274dd0bc8babb7d06687&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
           </video>
           <div>
             <strong>A Moving Truck</strong>
@@ -45,8 +44,7 @@ const videoWatchPage = () => {
           </div>
         </div>
         <div id="videoContent">
-          <video>
-            <source src="https://player.vimeo.com/external/374119382.sd.mp4?s=2e2cf9aa0bd93c53987df1a2b07240602942ce72&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          <video class="video" src="https://player.vimeo.com/external/374119382.sd.mp4?s=2e2cf9aa0bd93c53987df1a2b07240602942ce72&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
           </video>
           <div>
             <strong>A Moving Truck</strong>
@@ -56,8 +54,7 @@ const videoWatchPage = () => {
           </div>
         </div>
         <div id="videoContent">
-          <video>
-            <source src="https://player.vimeo.com/external/331083783.sd.mp4?s=d817f766244c10a344e39045edf595366cb72979&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          <video class="video" src="https://player.vimeo.com/external/331083783.sd.mp4?s=d817f766244c10a344e39045edf595366cb72979&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
           </video>
           <div>
             <strong>A Moving Truck</strong>
@@ -67,8 +64,7 @@ const videoWatchPage = () => {
           </div>
         </div>
         <div id="videoContent">
-          <video>
-            <source src="https://player.vimeo.com/external/363169958.sd.mp4?s=2bcfbdfcb641cd1668ea8cee4f038420d8b5cf8c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+          <video class="video" src="https://player.vimeo.com/external/363169958.sd.mp4?s=2bcfbdfcb641cd1668ea8cee4f038420d8b5cf8c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
           </video>
           <div>
             <strong>A Moving Truck</strong>

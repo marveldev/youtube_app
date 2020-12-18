a1390c309cb3c94601c9da874d142757f67e798b
Marvel Dev <marvellousalika48@gmail.com>
2020-12-18 07:49:41 +0100

    display settings page

--- modules/rightNav/events.js
+++ modules/rightNav/events.js
@@ -5,6 +5,12 @@ const rightNavEventListeners = () => {
   channelPage.addEventListener('click', () => {
     switchCurrentPage(constants.CHANNELPAGE);
   })
+
+  document.querySelector('#settingButton').addEventListener('click', () => {
+    switchCurrentPage(constants.MOBILESETTINGSPAGE)
+    overlay.style.display = 'none';
+    document.querySelector('.rightNav-modal').style.display = 'none';
+  })
 }
 
 export default rightNavEventListeners;

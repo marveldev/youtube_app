cf1b075694b6398d33de10f510221210275d2112
Marvel Dev <marvellousalika48@gmail.com>
2020-12-19 05:07:51 +0100

    add condition for display of currentpage

--- modules/rightNav/events.js
+++ modules/rightNav/events.js
@@ -1,16 +1,30 @@
+import defaultLeftNav from '../defaultLeftNav/defaultLeftNav.js';
+import defaultNavEventListeners from '../defaultLeftNav/events.js';
 import { switchCurrentPage, constants } from '../helpers.js';
 
 const rightNavEventListeners = () => {
-  const channelPage = document.querySelector('.channel');
-  channelPage.addEventListener('click', () => {
-    switchCurrentPage(constants.CHANNELPAGE);
+  document.querySelector('.channel').addEventListener('click', () => {
+    const modalLeftNav = localStorage.getItem('modalLeftNav');
+    const currentSideNav = document.querySelector('.side-nav');
+    const overlay = document.querySelector('#overlay');
+
+    if (modalLeftNav === 'true') {
+      switchCurrentPage(constants.CHANNELPAGE);
+      currentSideNav.innerHTML = defaultLeftNav();
+      defaultNavEventListeners();
+      document.querySelector('#main').className = 'grid-container';
+      document.querySelector('.rightNav-modal').style.display = 'none';
+      overlay.style.display = 'none';
+    } else {
+      switchCurrentPage(constants.CHANNELPAGE);
+    }
   })
 
-  document.querySelector('#settingButton').addEventListener('click', () => {
-    switchCurrentPage(constants.MOBILESETTINGSPAGE)
-    overlay.style.display = 'none';
-    document.querySelector('.rightNav-modal').style.display = 'none';
-  })
+  // document.querySelector('#settingButton').addEventListener('click', () => {
+  //   switchCurrentPage(constants.MOBILESETTINGSPAGE)
+  //   overlay.style.display = 'none';
+  //   document.querySelector('.rightNav-modal').style.display = 'none';
+  // })
 }
 
 export default rightNavEventListeners;

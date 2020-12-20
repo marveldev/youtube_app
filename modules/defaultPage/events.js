8a32139ef30dfb5eedea75076f112c8226a97416
Marvel Dev <marvellousalika48@gmail.com>
2020-12-20 04:45:36 +0100

    rename videoPage to videoWatchPage

--- modules/defaultPage/events.js
+++ modules/defaultPage/events.js
@@ -5,7 +5,7 @@ const defaultPageEventListeners = () => {
   for (let index = 0; index < videos.length; index++) {
     const video = videos[index];
     video.addEventListener('click', () => {
-      switchCurrentPage(constants.VIDEOPAGE, video.src);
+      switchCurrentPage(constants.VIDEOWATCHPAGE, video.src);
       localStorage.setItem('videoSrc', video.src)
     })
   }

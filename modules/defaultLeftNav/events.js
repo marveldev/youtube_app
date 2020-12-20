adce592a5e8204907ca63a610a83a7a1b45810b5
Marvel Dev <marvellousalika48@gmail.com>
2020-12-20 04:45:23 +0100

    add settings buttons event listener.
    - rename currentSide nav to left side nav

--- modules/defaultLeftNav/events.js
+++ modules/defaultLeftNav/events.js
@@ -7,15 +7,14 @@ const defaultNavEventListeners = () => {
       const selector = selectors[index];
       selector.addEventListener('click', () => {
         const modalLeftNav = localStorage.getItem('modalLeftNav');
-        const currentSideNav = document.querySelector('.side-nav');
-        const overlay = document.querySelector('#overlay');
-        if (modalLeftNav == 'true') {
+        const leftSideNav = document.querySelector('.side-nav');
+        if (modalLeftNav === 'true') {
           switchCurrentPage(constant);
-          currentSideNav.innerHTML = defaultLeftNav();
+          leftSideNav.innerHTML = defaultLeftNav();
           defaultNavEventListeners();
           document.querySelector('#main').className = 'grid-container';
           document.querySelector('.left-nav-modal').style.display = 'none';
-          overlay.style.display = 'none';
+          document.querySelector('#overlay').style.display = 'none';
         } else {
           switchCurrentPage(constant);
         }
@@ -25,8 +24,23 @@ const defaultNavEventListeners = () => {
 
   addSwitchPageEvent(document.querySelectorAll('.home'), constants.HOMEPAGE);
   addSwitchPageEvent(document.querySelectorAll('.trending'), constants.TRENDINGPAGE);
-  addSwitchPageEvent(document.querySelectorAll('.setting'), constants.SETTINGSPAGE);
   addSwitchPageEvent(document.querySelectorAll('.history'),constants.WATCHHISTORYPAGE);
+
+  const settingButtons = document.querySelectorAll('.setting');
+  for (let index = 0; index < settingButtons.length; index++) {
+    const settingButton = settingButtons[index];
+    settingButton.addEventListener('click', () => {
+      const modalLeftNav = localStorage.getItem('modalLeftNav');
+      if (modalLeftNav === 'true') {
+        switchCurrentPage(constants.SETTINGSPAGE);
+        document.querySelector('#main').className = 'grid-container';
+        document.querySelector('.left-nav-modal').style.display = 'none';
+        document.querySelector('#overlay').style.display = 'none';
+      } else {
+        switchCurrentPage(constants.SETTINGSPAGE);
+      }
+    })
+  }
 }
 
 export default defaultNavEventListeners;
